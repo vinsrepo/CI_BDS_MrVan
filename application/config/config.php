5641705600b57571619c0a4084f5acaac64dcb9a
HuuTx <huutx@vinsofts.net>
2019-11-08 18:38:39 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -16,10 +16,7 @@
 */ 
 
 
-// $config['base_url'] = 'http://sannhadat.vip';
-$root=(isset($_SERVER["HTTPS"]) ? "https://" : "http://").$_SERVER["HTTP_HOST"];
-$root.= str_replace(basename($_SERVER["SCRIPT_NAME"]), "", $_SERVER["SCRIPT_NAME"]);
-$config["base_url"] = $root; 
+$config['base_url'] = 'http://sannhadat.vip';
 $config['cookie_prefix']	= "";
 $config['cookie_domain']	= ".sannhadat.vip";
 $config['disk']	= '/';// CHU Y: Cai tren window thi ghi la C:
